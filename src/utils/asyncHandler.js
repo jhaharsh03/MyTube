d446029c3fcc61b2fe145c299d073cff7d3b268c
Harsh Jha <harshj92002@gmail.com>
2023-12-31 14:48:57 +0530

    Added utils files

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// this one is fore try and catch //
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             sucess: false,
+//             message: err.message
+//         })
+//     }
+// }
